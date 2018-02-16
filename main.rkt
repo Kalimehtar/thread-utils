b7ca8c844abe13716d2c1831cc5f359ebfa9513c
Roman Klochkov <kalimehtar@mail.ru>
2018-02-16 20:45:47 +0500

    thread-loop is a function

--- main.rkt
+++ main.rkt
@@ -1,45 +1,59 @@
 #lang racket/base
 (require racket/contract)
-(provide/contract [until-timeout (((-> any/c)) ((or/c #f (and/c real? (not/c negative?)) (-> any)) (any/c . -> . any))  . ->* . any/c)])
-(provide thread-loop)
+(provide/contract [until-timeout (((-> any/c)) ((or/c #f (and/c real? (not/c negative?)) (-> any)) (any/c . -> . any/c))  . ->* . any/c)]
+                  [thread-loop ((-> any) (any/c . -> . any) . -> . thread?)])
 
 (module+ test
   (require rackunit))
 
 (require racket/function)
 
-(define (until-timeout func [timeout 30] [on-error (const #t)])
+(define ((do-on-error on-error) e)
+  (if (procedure? on-error)
+      (with-handlers ([(const #t) (const #f)])
+        (on-error e))
+      on-error))
+
+(define (until-timeout func [timeout 30] [on-error #f])
   (call-in-nested-thread
    (λ ()
      (define master (current-thread))
-     (define slave (thread (λ () (thread-send master
-                                              (with-handlers ([(const #t) (λ (e) (on-error e) eof)])
-                                                (func))))))
+     (define slave
+       (thread (λ ()
+                 (thread-send master
+                              (with-handlers ([(const #t) (do-on-error on-error)])
+                                (func))))))
      (cond
        [(sync/timeout timeout slave)
         (thread-receive)]
        [else
         (kill-thread slave)
-        eof]))))
+        ((do-on-error on-error) #f)]))))
 
-(define-syntax-rule (thread-loop on-error BODY ...)
+(define (thread-loop func [on-error #f])
   (thread
    (λ ()
-     (with-handlers ([(const #t) on-error])
+     (with-handlers ([(const #t) (do-on-error on-error)])
        (let loop ()
-         BODY ...
+         (func)
          (loop))))))
 
 (module+ test
-  (check-eq? (until-timeout (thunk 'a) 1) 'a)
-  (check-eq? (until-timeout (thunk (sleep 2)) 1) eof)
-  (check-eq? (until-timeout (thunk (error 'ok)) 1) eof)
-  (check-eq? (until-timeout (thunk eof) 1) eof)
+  (check-eq? (until-timeout (thunk 'a) 0.1) 'a)
+  (check-eq? (until-timeout (thunk (sleep 0.2)) 0.1) #f)
+  (check-eq? (until-timeout (thunk (error 'ok)) 0.1) #f)
+  (check-eq? (until-timeout (thunk eof) 0.1) eof)
+  (check-eq? (until-timeout (thunk (error 'ok)) 0.1 eof) eof)
   (check-eq? (let ([err 'no])
                (define (on-error x) (set! err 'yes))
-               (until-timeout (thunk (error 'ok)) 1 on-error)
+               (until-timeout (thunk (error 'ok)) 0.1 on-error)
                err)
              'yes)
+  (check-eq? (let ([x 0])
+               (define t (thread-loop (thunk (set! x (add1 x)) (sleep 0.15))))
+               (sleep 0.2)
+               (kill-thread t)
+               x) 2)
   )
 
 (module+ main
--- scribblings/thread-utils.scrbl
+++ scribblings/thread-utils.scrbl
@@ -13,5 +13,10 @@ This package provides some utilities to do common patterns of async programming.
                         [timeout (or/c #f (and/c real? (not/c negative?)) (-> any)) 30]
                         [on-error (any/c . -> . any/c)]) any/c]{
 Calls @racket[func] and wait for answer not more then @racket[timeout].
-If @racket[func] fails to complete until that moment, returns @racket[eof]. If func raise an exception, applies @racket[on-error] to
-the exception}
+If @racket[func] fails to complete until that moment, returns @racket[(in-error #f)]. If func raise an exception,
+ applies @racket[on-error] to the exception and returns it's result.}
+
+@defproc[(thread-loop [func (-> any/c)] [on-error (any/c . -> . any)]) thread?]{
+Returns thread, which runs @racket[func] in infinite loop. When @racket[func] raises an exception, applies
+@racket[on-error] to the exception and run @racket[func] again.}
+
